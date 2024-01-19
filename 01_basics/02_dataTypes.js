442bb2a41cd06945ab284ea7737c32f8045a0528
Indrajeet Manoj Yadav <156185223+Indrajeetmyadav@users.noreply.github.com>
2024-01-19 12:12:26 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version.
+
+// alert(3+3) // we are using nodejs, not browser.
+
+console.log(3 + 3);
+
+/*Primitive DataTypes*/
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefine => not assign a value now
+// symbol => define uniqueness
+
+// object
+let name = "Indrajeet";
+let age = 27;
+let isLoggedIn = false;
+
+//Knowing the typeof any value 
+console.log(typeof "Indrajeet");//eg.string
+console.log(typeof null);// object
+console.log(typeof undefine);// undefine
+
