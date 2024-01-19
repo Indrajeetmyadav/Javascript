c5ae4f23c04d0befc2fe5787863b9307129d0a88
Indrajeet Manoj Yadav <156185223+Indrajeetmyadav@users.noreply.github.com>
2024-01-19 12:35:48 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "Indra"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" convert in => 33
+// "33abc" notconver => NaN (not a num)
+// true convert in => 1
+// "Indra" notconvert => NaN
+
+let isLoggedIn = "indrajeet"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=> false
+// "" => false
+// "indrajeet" => true
+
+let someNumber = 10
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
