9334f9a90c99f772025d7fed2d490b28876dd2b9
Indrajeet Manoj Yadav <156185223+Indrajeetmyadav@users.noreply.github.com>
2024-01-27 03:05:45 +0000

    Stack and Heap Memory in JavaScript

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -23,8 +23,38 @@ let myObj = {
     age: 28,
 }
 
-const myFunction = function(){
+const myFunction = function () {
     console.log("Hello world");
 }
 
 console.log(typeof myObj);
+/*************************************/
+
+// Stack (Primitive), Heap (Non-Primitive)
+/* In stack memory when we declare the variable 
+and assign the value and declare the another var 
+assign first value then we chage second var value in that case 
+we changing copy value change but original is safe.
+In stack memory we have a Copy.*/
+let myPagename = "indrajeetdotcom"// original
+
+let anothername = myPagename // copy
+anothername = "coffeeaurcode" // Chage with the copy.
+
+console.log(myPagename);// indrajeetdotcom
+console.log(anothername);// coffeeaurcode
+
+/*In heap memory we have a reference.
+In this we changining the original value.*/
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne 
+
+userTwo.email = "indrajeet@google.com" // original reference changed.
+
+console.log(userOne.email);
+console.log(userTwo.email);
